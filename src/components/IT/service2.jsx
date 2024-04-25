c711a744446eab5a9e37fe21f33b32a8e6cd68b5
unknown <divyaneela75@gmail.com>
2024-04-25 12:50:46 +0530

    Initial commit

--- src/components/IT/service2.jsx
+++ src/components/IT/service2.jsx
@@ -15,24 +15,24 @@ export const Service2 = () => {
             <div className='service2 p-5 pt-2'>
                 <div className='row'>
                     <div className='col-md-12 col-lg-8'>
-                        <div className='d-flex justify-content-center align-items-center' style={{flexDirection: 'column'}}>
-                            <p style={{color: '#888484'}}><span style={{fontSize: '24px', color: '#01057F'}}><span style={{fontWeight: '700'}}>Data</span> Analytic :</span> Unleash the power of your data with our
+                        <div className='d-flex justify-content-center align-items-center' style={{ flexDirection: 'column' }}>
+                            <p style={{ color: '#888484' }}><span style={{ fontSize: '24px', color: '#01057F' }}><span style={{ fontWeight: '700' }}>Data</span> Analytic :</span> Unleash the power of your data with our
                                 advanced analytics solutions. Gain valuable insights, make
                                 informed decisions, and stay ahead in today's data-driven world.
                             </p>
-                            <p style={{color: '#888484'}}><span style={{fontSize: '24px', color: '#01057F'}}><span style={{fontWeight: '700'}}>Enterprise</span> Solutions  :</span> Leverage the power
+                            <p style={{ color: '#888484' }}><span style={{ fontSize: '24px', color: '#01057F' }}><span style={{ fontWeight: '700' }}>Enterprise</span> Solutions  :</span> Leverage the power
                                 of the cloud with our scalable and secure solutions. Transform
                                 your operations, enhance flexibility, and achieve
                                 unparalleled business agility.</p>
-                            <p style={{color: '#888484'}}><span style={{fontSize: '24px', color: '#01057F'}}><span style={{fontWeight: '700'}}>IoT</span> & <span style={{fontWeight: '700'}}>Digital engineering</span>  :</span> Embrace the era of IoT and
+                            <p style={{ color: '#888484' }}><span style={{ fontSize: '24px', color: '#01057F' }}><span style={{ fontWeight: '700' }}>IoT</span> & <span style={{ fontWeight: '700' }}>Digital engineering</span>  :</span> Embrace the era of IoT and
                                 digital transformation. Our innovative solutions enable you to
                                 connect devices, analyse data, and drive intelligent decision-
                                 making.</p>
-                            <p style={{color: '#888484'}}><span style={{fontSize: '24px', color: '#01057F'}}><span style={{fontWeight: '700'}}>Network services</span> & <span style={{fontWeight: '700'}}>Solutions</span>  :</span> Ensure seamless
+                            <p style={{ color: '#888484' }}><span style={{ fontSize: '24px', color: '#01057F' }}><span style={{ fontWeight: '700' }}>Network services</span> & <span style={{ fontWeight: '700' }}>Solutions</span>  :</span> Ensure seamless
                                 connectivity and robust network infrastructure. Our solutions
                                 are designed to keep your business running smoothly in an
                                 ever-evolving digital landscape.</p>
-                            <p style={{color: '#888484'}}><span style={{fontSize: '24px', color: '#01057F'}}><span style={{fontWeight: '700'}}>Cyber</span>security  :</span> Ensure the integrity and confidentiality of your
+                            <p style={{ color: '#888484' }}><span style={{ fontSize: '24px', color: '#01057F' }}><span style={{ fontWeight: '700' }}>Cyber</span>security  :</span> Ensure the integrity and confidentiality of your
                                 digital assets with our robust cybersecurity solutions. Safeguard
                                 your business against evolving cyber threats with our cutting-edge
                                 security measures.</p>
@@ -40,7 +40,7 @@ export const Service2 = () => {
                     </div>
                     <div className='col-md-12 col-lg-4'>
                         <div className='d-flex justify-content-center align-items-center'>
-                            <img src={service2} alt='img' className='img-fluid' style={{width: '70%'}}/>
+                            <img src={service2} alt='img' className='img-fluid' style={{ width: '70%' }} />
                         </div>
                     </div>
                 </div>
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,6 +2,10 @@ import { React, useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../components/navbar.css';
 import logo from "../assets/logo_grd_RED AND GOLD_final 4.png";
+import home from '../assets/Home-page.png'
+import IT from '../assets/IT-page.png'
+import cyber from '../assets/Cyber-page.png'
+import buzzbee from '../assets/Buzzbee-page.png'
 
 
 
@@ -31,15 +35,15 @@ export const Navbar = () => {
                         <div className="nav-grid">
                             <div className="nav-column">
                                 <ul className="nav-links">
-                                    <li><Link to='/' onClick={toggleNav}><span>01</span>Home</Link></li>
-                                    <li><Link to='/IT' onClick={toggleNav}><span>02</span>IT</Link></li>
-                                    <li><Link to='/services' onClick={toggleNav}><span>03</span>Services</Link></li>
+                                    <li><Link to='/' onClick={toggleNav}><span>01</span>Home<img src={home} alt="home-img" className="nav-image" /></Link></li>
+                                    <li><Link to='/IT' onClick={toggleNav}><span>02</span>Services<img src={IT} alt="IT-img" className="nav-image" /></Link></li>
+                                    <li><Link to='/services' onClick={toggleNav}><span>03</span>Cybersecurity<img src={cyber} alt="cyber-img" className="nav-image" /></Link></li>
                                 </ul>
                             </div>
                             <div className="nav-column">
                                 <ul className="nav-links">
-                                    <li><Link to='/services' onClick={toggleNav}><span>04</span>Blog</Link></li>
-                                    <li><Link to='/contact' onClick={toggleNav}><span>05</span>Contact</Link></li>
+                                    <li><Link to='/services' onClick={toggleNav}><span>04</span>BuzzBee<img src={buzzbee} alt="buzzbee-img" className="nav-image" /></Link></li>
+                                    {/* <li><Link to='/contact' onClick={toggleNav}><span>05</span>Contact<img src='' alt="contact-img" className="nav-image" /></Link></li> */}
                                 </ul>
                             </div>
                         </div>
