2b8c1282ab6415328b79b40c2417bdfbfce76ab6
unknown <divyaneela75@gmail.com>
2024-05-08 18:35:53 +0530

    Initial commit

--- src/components/Buzzbee/buzzbee3.jsx
+++ src/components/Buzzbee/buzzbee3.jsx
@@ -20,10 +20,10 @@ export const Buzzbee3 = () => {
                     </div>
                     <div className='col-md-9 col-12'>
                         <div className='d-flex justify-content-around align-items-center gap-5 buzz-gif-container p-3 w-100'>
-                            <div className='w-25'>
+                            <div className='w-25 buzz-gif-img'>
                                 <img src={gif1} alt="GIF" className='img-fluid' style={{borderRadius: '10px'}}/>
                             </div>
-                            <div className='w-75'>
+                            <div className='w-75 buzz-gif-cont' >
                                 <h5>Branding Strategy Services</h5>
                                 <p>Brand Creation Brand Identity, Insights & Positioning: Messaging & Narratives Brand Experiences Campaign Development</p>
                             </div>
@@ -38,10 +38,10 @@ export const Buzzbee3 = () => {
                     </div>
                     <div className='col-md-9 col-12'>
                         <div className='d-flex justify-content-around align-items-center gap-5 buzz-gif-container p-3 w-100'>
-                            <div className=' w-25'>
+                            <div className=' w-25 buzz-gif-img'>
                                 <img src={gif2} alt="GIF" className='img-fluid' style={{borderRadius: '10px'}}/>
                             </div>
-                            <div className='w-75'>
+                            <div className='w-75 buzz-gif-cont'>
                                 <h5>Advertising Solutions</h5>
                                 <p>Television Commercials, Radio Celebrity Endorsements Print Media, Media Buying, Outdoor Branding, door advertising, Strategy Planning, Paper Production</p>
                             </div>
@@ -56,10 +56,10 @@ export const Buzzbee3 = () => {
                     </div>
                     <div className='col-md-9 col-12'>
                         <div className='d-flex justify-content-around align-items-center gap-5 buzz-gif-container p-3 w-100'>
-                            <div className=' w-25'>
+                            <div className=' w-25 buzz-gif-img'>
                                 <img src={gif3} alt="GIF" className='img-fluid' style={{borderRadius: '10px'}}/>
                             </div>
-                            <div className='w-75'>
+                            <div className='w-75 buzz-gif-cont'>
                                 <h5>Digital Media Marketing</h5>
                                 <p>Social Media Marketing, Lead Generation , Content Marketing, Search Engine Marketing, Influencer Marketing, Email / WhatsApp / Text Marketing, Search Engine Optimization</p>
                             </div>
@@ -74,10 +74,10 @@ export const Buzzbee3 = () => {
                     </div>
                     <div className='col-md-9 col-12'>
                         <div className='d-flex justify-content-around align-items-center gap-5 buzz-gif-container p-3 w-100'>
-                            <div className=' w-25'>
+                            <div className=' w-25 buzz-gif-img'>
                                 <img src={gif4} alt="GIF" className='img-fluid' style={{borderRadius: '10px'}}/>
                             </div>
-                            <div className='w-75'>
+                            <div className='w-75 buzz-gif-cont'>
                                 <h5>Printing & Design</h5>
                                 <p>Brochures & Catalogues Pitch Decks Newsletter Annual Report Company Profile Calendar Design Flyers & Posters</p>
                             </div>
@@ -92,10 +92,10 @@ export const Buzzbee3 = () => {
                     </div>
                     <div className='col-md-9 col-12'>
                         <div className='d-flex justify-content-around align-items-center gap-5 buzz-gif-container p-3 w-100'>
-                            <div className=' w-25'>
+                            <div className=' w-25 buzz-gif-img'>
                                 <img src={gif5} alt="GIF" className='img-fluid' style={{borderRadius: '10px'}}/>
                             </div>
-                            <div className='w-75'>
+                            <div className='w-75 buzz-gif-cont'>
                                 <h5>Packaging Solution</h5>
                                 <p>Moduled Packaging Design Product Packaging Design Packaging Label Design Identical Packaging Design Secure Packaging Design Bottle Packaging Design</p>
                             </div>
@@ -110,10 +110,10 @@ export const Buzzbee3 = () => {
                     </div>
                     <div className='col-md-9 col-12'>
                         <div className='d-flex justify-content-around align-items-center gap-5 buzz-gif-container p-3 w-100'>
-                            <div className=' w-25'>
+                            <div className=' w-25 buzz-gif-img'>
                                 <img src={gif6} alt="GIF" className='img-fluid' style={{borderRadius: '10px'}}/>
                             </div>
-                            <div className='w-75'>
+                            <div className='w-75 buzz-gif-cont'>
                                 <h5>proposal, Pitch & Designing</h5>
                                 <p>Business Proposal Pitch Deck Company Profile Annual Report Corporate Presentation Official Documentst</p>
                             </div>
@@ -128,10 +128,10 @@ export const Buzzbee3 = () => {
                     </div>
                     <div className='col-md-9 col-12'>
                         <div className='d-flex justify-content-around align-items-center gap-5 buzz-gif-container p-3 w-100'>
-                            <div className=' w-25'>
+                            <div className=' w-25 buzz-gif-img'>
                                 <img src={gif8} alt="GIF" className='img-fluid' style={{borderRadius: '10px'}}/>
                             </div>
-                            <div className='w-75 '>
+                            <div className='w-75 buzz-gif-cont'>
                                 <h5>Visual strategies</h5>
                                 <p>Explainer Videos Documentaries Photography Corporate Videos Videography Animation Videos</p>
                             </div>
@@ -146,10 +146,10 @@ export const Buzzbee3 = () => {
                     </div>
                     <div className='col-md-9 col-12'>
                         <div className='d-flex justify-content-around align-items-center gap-5 buzz-gif-container p-3 w-100'>
-                            <div className=' w-25'>
+                            <div className=' w-25 buzz-gif-img'>
                                 <img src={gif8} alt="GIF" className='img-fluid' style={{borderRadius: '10px'}}/>
                             </div>
-                            <div className='w-75'>
+                            <div className='w-75 buzz-gif-cont'>
                                 <h5>Content Generation</h5>
                                 <p>Website Content Branding Content Social Media Content Translation Copywriting</p>
                             </div>
