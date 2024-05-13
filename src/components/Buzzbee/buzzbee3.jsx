8030f97ae0b6c3c224e8e90090c15d47e77af755
unknown <divyaneela75@gmail.com>
2024-05-13 12:25:51 +0530

    Initial commit

--- src/components/Buzzbee/buzzbee3.jsx
+++ src/components/Buzzbee/buzzbee3.jsx
@@ -24,7 +24,7 @@ export const Buzzbee3 = () => {
                                 <img src={gif1} alt="GIF" className='img-fluid' style={{borderRadius: '10px'}}/>
                             </div>
                             <div className='w-75 buzz-gif-cont' >
-                                <h5>Branding Strategy Services</h5>
+                                <h5 style={{color: 'black'}}>Branding Strategy Services</h5>
                                 <p>Brand Creation Brand Identity, Insights & Positioning: Messaging & Narratives Brand Experiences Campaign Development</p>
                             </div>
                         </div>
@@ -42,7 +42,7 @@ export const Buzzbee3 = () => {
                                 <img src={gif2} alt="GIF" className='img-fluid' style={{borderRadius: '10px'}}/>
                             </div>
                             <div className='w-75 buzz-gif-cont'>
-                                <h5>Advertising Solutions</h5>
+                                <h5 style={{color: 'black'}}>Advertising Solutions</h5>
                                 <p>Television Commercials, Radio Celebrity Endorsements Print Media, Media Buying, Outdoor Branding, door advertising, Strategy Planning, Paper Production</p>
                             </div>
                         </div>
@@ -60,7 +60,7 @@ export const Buzzbee3 = () => {
                                 <img src={gif3} alt="GIF" className='img-fluid' style={{borderRadius: '10px'}}/>
                             </div>
                             <div className='w-75 buzz-gif-cont'>
-                                <h5>Digital Media Marketing</h5>
+                                <h5 style={{color: 'black'}}>Digital Media Marketing</h5>
                                 <p>Social Media Marketing, Lead Generation , Content Marketing, Search Engine Marketing, Influencer Marketing, Email / WhatsApp / Text Marketing, Search Engine Optimization</p>
                             </div>
                         </div>
@@ -78,7 +78,7 @@ export const Buzzbee3 = () => {
                                 <img src={gif4} alt="GIF" className='img-fluid' style={{borderRadius: '10px'}}/>
                             </div>
                             <div className='w-75 buzz-gif-cont'>
-                                <h5>Printing & Design</h5>
+                                <h5 style={{color: 'black'}}>Printing & Design</h5>
                                 <p>Brochures & Catalogues Pitch Decks Newsletter Annual Report Company Profile Calendar Design Flyers & Posters</p>
                             </div>
                         </div>
@@ -96,7 +96,7 @@ export const Buzzbee3 = () => {
                                 <img src={gif5} alt="GIF" className='img-fluid' style={{borderRadius: '10px'}}/>
                             </div>
                             <div className='w-75 buzz-gif-cont'>
-                                <h5>Packaging Solution</h5>
+                                <h5 style={{color: 'black'}}>Packaging Solution</h5>
                                 <p>Moduled Packaging Design Product Packaging Design Packaging Label Design Identical Packaging Design Secure Packaging Design Bottle Packaging Design</p>
                             </div>
                         </div>
@@ -114,7 +114,7 @@ export const Buzzbee3 = () => {
                                 <img src={gif6} alt="GIF" className='img-fluid' style={{borderRadius: '10px'}}/>
                             </div>
                             <div className='w-75 buzz-gif-cont'>
-                                <h5>proposal, Pitch & Designing</h5>
+                                <h5 style={{color: 'black'}}>proposal, Pitch & Designing</h5>
                                 <p>Business Proposal Pitch Deck Company Profile Annual Report Corporate Presentation Official Documentst</p>
                             </div>
                         </div>
@@ -132,7 +132,7 @@ export const Buzzbee3 = () => {
                                 <img src={gif8} alt="GIF" className='img-fluid' style={{borderRadius: '10px'}}/>
                             </div>
                             <div className='w-75 buzz-gif-cont'>
-                                <h5>Visual strategies</h5>
+                                <h5 style={{color: 'black'}}>Visual strategies</h5>
                                 <p>Explainer Videos Documentaries Photography Corporate Videos Videography Animation Videos</p>
                             </div>
                         </div>
@@ -150,7 +150,7 @@ export const Buzzbee3 = () => {
                                 <img src={gif8} alt="GIF" className='img-fluid' style={{borderRadius: '10px'}}/>
                             </div>
                             <div className='w-75 buzz-gif-cont'>
-                                <h5>Content Generation</h5>
+                                <h5 style={{color: 'black'}}>Content Generation</h5>
                                 <p>Website Content Branding Content Social Media Content Translation Copywriting</p>
                             </div>
                         </div>
