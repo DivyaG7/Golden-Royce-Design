1c4d8a1fe9f46473d9c8f80416c0208cdd0471a2
unknown <divyaneela75@gmail.com>
2024-05-16 15:10:16 +0530

    Initial commit

--- src/components/home/home-component3.jsx
+++ src/components/home/home-component3.jsx
@@ -19,7 +19,7 @@ export const Contact = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         console.log(formData);
-        axios.post('https://www.goldenroycedesign.com/server', formData)
+        axios.post('http://localhost:8001/form', formData)
             .then((res) => {
                 console.log(res);
                 // Reset form data
