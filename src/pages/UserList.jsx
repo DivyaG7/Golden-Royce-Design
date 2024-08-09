083271d4cb8aeffdaf8e0cdc15d37aeb2e27c82e
unknown <divyaneela75@gmail.com>
2024-08-09 15:54:04 +0530

    Initial commit

--- src/pages/UserList.jsx
+++ src/pages/UserList.jsx
@@ -9,7 +9,7 @@ const UserList = () => {
   useEffect(() => {
     const fetchUsers = async () => {
       try {
-        const response = await axios.get('https://golden-royce-design.onrender.com/api/users');
+        const response = await axios.get('https://golden-royce-design.onrender.com/api/users'); // Ensure URL is correct
         setUsers(response.data);
       } catch (err) {
         setError('An error occurred while fetching user details.');
@@ -29,8 +29,8 @@ const UserList = () => {
     <div>
       <h2>Registered Users</h2>
       <ul>
-        {users.map((user, index) => (
-          <li key={index}>{user.name} ({user.email})</li>
+        {users.map((user) => (
+          <li key={user._id}>{user.name} ({user.email})</li>
         ))}
       </ul>
     </div>
