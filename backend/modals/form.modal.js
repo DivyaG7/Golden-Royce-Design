25650190b4e7cd30efa683e1924458a2ffa30732
unknown <divyaneela75@gmail.com>
2024-08-09 16:15:30 +0530

    Initial commit

--- backend/modals/form.modal.js
+++ backend/modals/form.modal.js
@@ -21,22 +21,23 @@
 
 
 const mongoose = require('mongoose');
-const schema = mongoose.Schema;
+const Schema = mongoose.Schema;
 
-const formSchema = new schema({
-    name: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    }
+const formSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  }
 }, {
-    timestamps: true
+  timestamps: true
 });
 
 const Form = mongoose.model('FormData', formSchema);
 
 module.exports = Form;
+
